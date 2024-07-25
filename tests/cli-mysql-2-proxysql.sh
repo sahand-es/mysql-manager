6b3f61c740faf98fcee8edd79a7bbb59342d1303
aliakbar <aliakbar@hamravesh.com>
2024-07-25 19:43:27 +0000

    Correct image tag and tests

--- tests/cli-mysql-2-proxysql.sh
+++ tests/cli-mysql-2-proxysql.sh
@@ -14,13 +14,16 @@ docker compose exec mysql-s1 mysql -uroot -proot -e "select * from sales.t1;"
 echo -e "\n\nChecking replica..."
 docker compose exec mysql-s2 mysql -uroot -proot -e "select * from sales.t1;"
 
+echo -e "\n\nChecking events in master..."
+docker compose exec mysql-s1 mysql -uroot -proot -e "USE mysql; SHOW EVENTS;"
+
+
 echo -e "\n\nChecking proxysql config and stats..."
 sleep 10
 docker compose exec mysql-s1 mysql -uradmin -ppwd -h proxysql -P6032 -e "select * from runtime_mysql_servers;"
 docker compose exec mysql-s1 mysql -uradmin -ppwd -h proxysql -P6032 -e "SELECT * FROM monitor.mysql_server_connect_log ORDER BY time_start_us DESC LIMIT 6"
 docker compose exec mysql-s1 mysql -uradmin -ppwd -h proxysql -P6032 -e "select Queries, srv_host from stats_mysql_connection_pool\G"
 docker compose exec mysql-s1 mysql -uradmin -ppwd -h proxysql -P6032 -e "select * from stats_mysql_query_rules"
-docker compose exec mysql-s1 mysql -uroot -proot -e "USE mysql; SHOW EVENTS;"
 
 echo -e "\n\nChecking metrics from exporter..."
 curl localhost:9104/metrics | grep mysql_up

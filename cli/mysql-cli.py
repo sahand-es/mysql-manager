69e851b477b2ed10012f1391d64ca90b4cf419f0
aliakbar <aliakbar@hamravesh.com>
2024-12-08 08:33:51 +0000

    Delete unneeded code from mysql-cli

--- cli/mysql-cli.py
+++ cli/mysql-cli.py
@@ -25,30 +25,6 @@ etcd_client = EtcdClient()
 cluster_data_handler = ClusterDataHandler()
 logger = logging.getLogger(__name__)
 
-def read_config_file(config_file):
-    config = ConfigParser()
-    config.read(config_file)
-    return config
-
-
-def get_option_or_config(option, config, section, key, default=None):
-    if option:
-        return option
-    if config and config.has_option(section, key):
-        return config.get(section, key)
-    if default:
-        return default
-    raise click.BadParameter(f'Missing required parameter: {key}')
-
-
-def get_instance_from_config(config, name):
-    return {
-        'host': config.get(name, 'host'),
-        'user': config.get(name, 'user'),
-        'password': config.get(name, 'password'),
-        'port': int(config.get(name, 'port'))
-    }
-
 
 @click.group()
 @click.pass_context
@@ -131,77 +107,6 @@ def get_cluster_status():
     print("replica="+state.get("replica"))
 
 
-@mysql.command()
-@click.option('-n', '--name', help='MySQL name', required=True)
-@click.option('-h', '--host', help='MySQL host', required=True)
-@click.option('-u', '--user', help='Username for MySQL', default='root')
-@click.option('-p', '--password', help='Password for MySQL', required=True)
-@click.option('--port', help='Port for MySQL', type=int, default=3306)
-@click.pass_context
-def add(ctx, name, host, user, password, port):
-    config = ConfigParser()
-    config.read(DEFAULT_CONFIG_PATH)
-
-    if not config.has_section(name):
-        config.add_section(name)
-
-    config.set(name, 'host', host)
-    config.set(name, 'user', user)
-    config.set(name, 'password', password)
-    config.set(name, 'port', str(port))
-
-    with open(DEFAULT_CONFIG_PATH, 'w') as configfile:
-        config.write(configfile)
-
-
-
-@mysql.command()
-@click.option('-n', '--name', help='MySQL name')
-@click.option('-u', '--user', help='Username for new user')
-@click.option('-p', '--password', help='Password for new user')
-@click.option('--roles', help='Comma-separated roles for the new user')
-@click.pass_context
-def create_user(ctx, name, user, password, roles):
-    config = ctx.obj['CONFIG']
-
-    print(f"Creating user '{user}' with roles {roles.split(',')} on mysql '{name}'")
-    ins = Mysql(**get_instance_from_config(config, name))
-    ins.create_new_user(user, password, roles.split(','))
-
-
-def create_config_file_from_env(nodes_count: int):
-    filename = "/etc/mm/config.ini"
-    # if os.path.isfile(filename):
-    #     return
-    
-    config = ConfigParser()
-    config.add_section("mysql-s1")
-    config.set("mysql-s1", "host", os.getenv("MYSQL_S1_HOST"))
-    config.set("mysql-s1", "user", "root")
-    config.set("mysql-s1", "password", os.getenv("MYSQL_ROOT_PASSWORD"))
-    if nodes_count == 2:
-        config.add_section("mysql-s2")
-        config.set("mysql-s2", "host", os.getenv("MYSQL_S2_HOST"))
-        config.set("mysql-s2", "user", "root")
-        config.set("mysql-s2", "password", os.getenv("MYSQL_ROOT_PASSWORD"))
-    
-    config.add_section("proxysql-1")
-    config.set("proxysql-1", "host", os.getenv("PROXYSQL_HOST"))
-    config.set("proxysql-1", "user", "radmin")
-    config.set("proxysql-1", "password", os.getenv("PROXYSQL_PASSWORD"))
-
-    config.add_section("users")
-    config.set("users", "repl_password", os.getenv("MYSQL_REPL_PASSWORD"))
-    config.set("users", "exporter_password", os.getenv("MYSQL_EXPORTER_PASSWORD"))
-    config.set("users", "proxysql_mon_password", os.getenv("PROXYSQL_MON_PASSWORD"))
-    config.set("users", "nonpriv_user", os.getenv("MYSQL_NONPRIV_USER"))
-    config.set("users", "nonpriv_password", os.getenv("MYSQL_NONPRIV_PASSWORD"))
-
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, "w") as configfile:
-        config.write(configfile)
-
-
 def signal_handler(signum, frame):
     raise ProgramKilled()
 
@@ -218,83 +123,6 @@ def run():
     except ProgramKilled:
         print("Received termination signal. Exiting...")
 
-@cli.group()
-@click.pass_context
-def proxysql(ctx):
-    pass
-
-
-@proxysql.command()
-@click.option('-n', '--name', help='MySQL name', required=True)
-@click.option('-h', '--host', help='MySQL host', required=True)
-@click.option('-u', '--user', help='Username for MySQL', default='root')
-@click.option('-p', '--password', help='Password for MySQL', required=True)
-@click.option('--port', help='Port for MySQL', type=int, default=6032)
-@click.pass_context
-def add(ctx, name, host, user, password, port):
-    config = ConfigParser()
-    config.read(DEFAULT_CONFIG_PATH)
-
-    if not config.has_section(name):
-        config.add_section(name)
-
-    config.set(name, 'host', host)
-    config.set(name, 'user', user)
-    config.set(name, 'password', password)
-    config.set(name, 'port', str(port))
-
-    with open(DEFAULT_CONFIG_PATH, 'w') as configfile:
-        config.write(configfile)
-
-
-@proxysql.command()
-@click.option('-n', '--name', help='ProxySQL name')
-@click.option('--mysql-user', help='MySQL username')
-@click.option('--mysql-password', help='MySQL password')
-@click.option('--monitor-user', help='Monitor username')
-@click.option('--monitor-password', help='Monitor password')
-@click.pass_context
-def initialize(ctx, name, mysql_user, mysql_password, monitor_user, monitor_password):
-    config = ctx.obj['CONFIG']
-
-    proxysql_ins = get_instance_from_config(config, name)
-    proxysql_ins.pop('port')
-
-    print(f"Initializing ProxySQL on proxysql '{name}'")
-    px = ProxySQL(
-        **proxysql_ins,
-        mysql_user=mysql_user,
-        mysql_password=mysql_password,
-        monitor_user=monitor_user,
-        monitor_password=monitor_password,
-    )
-    px.initialize_setup()
-
-
-@proxysql.command()
-@click.option('--mysql-name', help='MySql name')
-@click.option('--proxysql-name', help='MySql name')
-@click.option('--read-weight', help='Read weight')
-@click.option('--is-writer', is_flag=True, help='Specify if the backend is a writer')
-@click.pass_context
-def add_backend(ctx, mysql_name, proxysql_name, read_weight, is_writer):
-    config = ctx.obj['CONFIG']
-
-    role = 'writer' if is_writer else 'reader'
-    print(f"Adding {role} backend to ProxySQL on {proxysql_name}'")
-
-    mysql_ins = get_instance_from_config(config, mysql_name)
-    mysql_instance = Mysql(**mysql_ins)
-    proxysql_ins = get_instance_from_config(config, proxysql_name)
-    proxysql_ins.pop('port')
-    px = ProxySQL(
-        **proxysql_ins,
-        mysql_user=mysql_ins.get('user'),
-        mysql_password=mysql_ins.get('password'),
-        monitor_user='',
-        monitor_password='',
-    )
-    px.add_backend(mysql_instance, read_weight, is_writer)
 
 if __name__ == '__main__':
     cli()
